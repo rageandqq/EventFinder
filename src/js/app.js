6f6bad462b8c3026767591e0ff92d7388a48e8df
rageandqq <chitley.sameer@gmail.com>
2015-03-18 21:56:53 -0400

    Added location and more styling.

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,20 @@
 var app = angular.module('EventbriteApp', []);
 
-app.controller('MainController', ['$scope', function($scope) {
+app.controller('MainController', ['$scope', '$timeout', function($scope, $timeout) {
+
   $scope.name = 'World';
+  $scope.userPosition = null;
+
+  var geoHandler = function(position) {
+    $timeout(function() {
+      $scope.userPosition = position;
+    });
+  };
+
+  function init() {
+    navigator.geolocation.getCurrentPosition(geoHandler);
+  };
+
+  init();
+
 }]);
